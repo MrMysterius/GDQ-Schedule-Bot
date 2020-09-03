85acf2f1d9d71d8f2fb55e10795ff6caa9a1f20e
Mr Mysterius <25854814+MrMysterius@users.noreply.github.com>
2020-09-03 14:33:29 +0200

    Cleaning Up Code

--- index.js
+++ index.js
@@ -67,33 +67,6 @@ function getAccessToken(oAuth2Client, callback) {
   });
 }
 
-/**
- * Lists the next 10 events on the user's primary calendar.
- * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
- */
-function listEvents(auth) {
-  const calendar = google.calendar({version: 'v3', auth});
-  calendar.events.list({
-    calendarId: 'ou7502s2f9tmmqqv4np8h9sn08@group.calendar.google.com',
-    timeMin: (new Date()).toISOString(),
-    maxResults: 200,
-    singleEvents: true,
-    orderBy: 'startTime',
-  }, (err, res) => {
-    if (err) return console.log('The API returned an error: ' + err);
-    const events = res.data.items;
-    if (events.length) {
-      console.log('Upcoming 200 events:');
-      events.map((event, i) => {
-        const start = event.start.dateTime || event.start.date;
-        console.log(`${start} - ${event.summary}`);
-      });
-    } else {
-      console.log('No upcoming events found.');
-    }
-  });
-}
-
 function start(auth) {
     const bot = new Bot(auth);
 }
